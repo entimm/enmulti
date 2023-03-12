2076759b1e799301d3a5f0b555c80e0dae2d3d6d
entimm <entimm@gmail.com>
2023-03-12 22:21:49 +0800

    上装饰函数

--- my_transformations.py
+++ my_transformations.py
@@ -1,10 +1,32 @@
 import base64
-import codecs
 import hashlib
 
-MY_TRANSFORMATIONS = (
-    ("MD5", "md5", lambda v, i, secs: hashlib.md5(v.encode()).hexdigest(),
-    ("Reverse", "reverse", lambda v, i, secs: v[::-1],
-    ("Base64 Encode", "base64_encode", lambda v, i, secs: base64.b64encode(v.encode()).decode(),
-    ("Base64 Decode", "base64_decode", lambda v, i, secs: base64.b64decode(v.encode()).decode()
-)
+
+def transformation(name, key):
+    def wrapper(func):
+        MY_TRANSFORMATIONS.append((name, key, func))
+        return func
+    return wrapper
+
+
+MY_TRANSFORMATIONS = []
+
+
+@transformation("MD5", "md5")
+def md5_transform(v, i, secs):
+    return hashlib.md5(v.encode()).hexdigest()
+
+
+@transformation("Reverse", "reverse")
+def reverse_transform(v, i, secs):
+    return v[::-1]
+
+
+@transformation("Base64 Encode", "base64_encode")
+def base64_encode_transform(v, i, secs):
+    return base64.b64encode(v.encode()).decode()
+
+
+@transformation("Base64 Decode", "base64_decode")
+def base64_decode_transform(v, i, secs):
+    return base64.b64decode(v.encode()).decode()

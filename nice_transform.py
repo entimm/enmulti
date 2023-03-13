186b1478e7f35c1b18c516a9801539b78495ab4c
entimm <entimm@gmail.com>
2023-03-13 08:47:47 +0800

    命名优化

--- nice_transform.py
+++ nice_transform.py
@@ -3,9 +3,9 @@ import sublime_plugin
 import hashlib
 import base64
 
-from .my_transformations import MY_TRANSFORMATIONS as TRANSFORMATIONS
+from .nice_transform_funcs import MY_TRANSFORMATIONS as TRANSFORMATIONS
 
-class TransformTextStartCommand(sublime_plugin.ApplicationCommand):
+class NiceTransformSelectCommand(sublime_plugin.ApplicationCommand):
     def run(self):
         # 提取菜单项名称
         options = [name for name, _, _ in TRANSFORMATIONS]
@@ -14,12 +14,12 @@ class TransformTextStartCommand(sublime_plugin.ApplicationCommand):
                 # 提取转换类型，并执行转换命令
                 transform_type = TRANSFORMATIONS[index][1]
                 sublime.active_window().run_command(
-                    "transform_text", {"transform_type": transform_type}
+                    "nice_transform", {"transform_type": transform_type}
                 )
         # 在快速选择面板中显示所有转换选项
         sublime.active_window().show_quick_panel(options, on_done)
 
-class TransformTextCommand(sublime_plugin.TextCommand):
+class NiceTransformCommand(sublime_plugin.TextCommand):
     def run(self, edit, transform_type=""):
         selected_texts = [self.view.substr(region) for region in self.view.sel()]
         for i, region in enumerate(self.view.sel()):
